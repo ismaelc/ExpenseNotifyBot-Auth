48fc5bc83709238877d674eb3bc2e9e6aadc04ab
ismaelc <chrisi@13mbp-11334.gateway.pace.com>
2017-03-05 17:39:01 -0800

    test

--- index.js
+++ index.js
@@ -137,9 +137,8 @@ app.get('/oauth2callback', function(request, response) {
                                      .then(function () {
                                          return queue.putMessage('js-queue-items-for-bot',
                                              new Buffer(JSON.stringify(message)).toString('base64'),
-
                                              {
-                                                 visibilityTimeout: 10     // Visible after 10 seconds
+                                                 //visibilityTimeout: 10     // Visible after 10 seconds
                                                  //messageTTL: 60 * 60 // Expires after 60 secs
                                              }
                                              //{}
