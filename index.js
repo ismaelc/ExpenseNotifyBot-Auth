d69b2e518e024e7c21072088c9f95e93bc493968
ismaelc <chrisi@13mbp-11334.gateway.pace.com>
2017-03-10 16:42:05 -0800

    test

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.get('/oauth2callback_concur', function(request, response) {
                     };
 
                     var message = {
-                        'address': state,
+                        'address': address,
                         'payload': payload
                     }
 
